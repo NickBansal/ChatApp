6167975119e0cb4d84e15e83804b38827230e765
Nick Bansal <extreme_wired@msn.com>
2019-07-15 16:39:41 +0100

    ejs file connected

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'), bodyParser.json());
+
+app.get('/', (req, res) => res.render('index'));
 
 
 module.exports = app;
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('.');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`You are listening in ${port}`);
