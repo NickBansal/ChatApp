21b38149b4fb23053ab6300f837b19d6c485c4c8
Nick Bansal <extreme_wired@msn.com>
2019-07-24 15:55:21 +0100

    disabled send location button

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,39 @@
 // eslint-disable-next-line no-undef
 const socket = io();
 
+const $form = document.querySelector('#form');
+const $locationButton = document.querySelector('#location');
+const $formButton = document.querySelector('#submit');
+const $input = document.querySelector('#input');
+
 socket.on('message', (message) => {
   console.log(message);
 });
 
-document.querySelector('#form').addEventListener('submit', (e) => {
+$form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  $formButton.setAttribute('disabled', 'disabled');
+
   socket.emit('sendMessage', e.target[0].value, (message) => {
     console.log(message);
+    e.target[0].value = '';
+    $formButton.removeAttribute('disabled');
+    $input.focus();
   });
-  e.target[0].value = '';
 });
 
-document.querySelector('#location').addEventListener('click', () => {
+
+$locationButton.addEventListener('click', () => {
+  $locationButton.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     console.log(position.coords.latitude, position.coords.longitude);
     socket.emit('sendLocation', {
       lat: position.coords.latitude,
       long: position.coords.longitude,
-    }, message => console.log(message));
+    }, (message) => {
+      console.log(message);
+      $locationButton.removeAttribute('disabled');
+    });
   });
 });
